9cd7ac6fae5e90da04f7439f743c32221e451a8c
Guilherme Andrade <42524262+guiandradedev@users.noreply.github.com>
2023-08-14 08:58:10 -0300

    first commit

---
+++ main.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
+#include <locale.h>
+
+int main()
+{
+    system("cls");
+    setlocale(LC_ALL, "Portuguese");
+
+    int opcao = 0, numVoos = 0;
+
+    do
+    {
+        system("cls");
+
+        printf("\n\t===\t|Menu|\t===\t\n");
+        printf("\n1. Opcao");
+        printf("\n2. \tSair\n");
+
+        scanf("%i", &opcao);
+        system("cls");
+
+        switch (opcao)
+        {
+        case 1:
+            break;
+
+        default:
+            break;
+        }
+    } while (opcao != 0 && opcao != 2);
+}
