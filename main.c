b62a93a44beaff837f0399a6f8d66568a43204a7
Ximenes <135142421+Ximenninho@users.noreply.github.com>
2023-08-14 09:36:03 -0300

    feat main.c

--- main.c
+++ main.c
@@ -3,24 +3,25 @@
 #include <string.h>
 #include <ctype.h>
 #include <locale.h>
+#include <math.h>
 
 int main()
 {
-    system("cls");
+    float a=0,b=0,c=0;
     setlocale(LC_ALL, "Portuguese");
 
-    int opcao = 0, numVoos = 0;
+    int opcao = 0;
 
     do
     {
         system("cls");
+        printf("========================\n");
+        printf("========Calculadoa======\n");
+        printf("========================\n");
 
-        printf("\n\t===\t|Menu|\t===\t\n");
-        printf("\n1. Opcao");
-        printf("\n2. \tSair\n");
+        printf("[1]Soma\n[2]Subtração\n[3]Multiplicação\n[4]Divisão\n[5]Raiz Quadrada\n[6]Potência\n");
 
-        scanf("%i", &opcao);
-        system("cls");
+        scanf("%d", &opcao);
 
         switch (opcao)
         {
