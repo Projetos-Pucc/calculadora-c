2bb011d8c62b9bafefa770a9f1cee5bc49b94f91
Luiza-CLopes <luizacaissutti@gmail.com>
2023-08-16 08:26:11 -0300

    Update main.c

--- main.c
+++ main.c
@@ -13,10 +13,13 @@ void le(int a, int b){
     printf("Quais os números? (Ex: 13 34)\n");
     scanf ("2%d", a, b); 
 }
+
 float soma (float a, float b) {
-    a + b = c;
+    float c; 
+    c = a + b;
     return c;
 }
+
 int main()
 {
     float a,b,c;
