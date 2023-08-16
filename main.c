96fd3520581ab8b14efdb9522a70eba63ac5ad8c
Ximenes <135142421+Ximenninho@users.noreply.github.com>
2023-08-16 09:06:45 -0300

    main.c

--- main.c
+++ main.c
@@ -9,16 +9,14 @@ void imp(float a, float b, float c, char op){
     printf ("Resultado: %.2f", c);
 }
 
-void le(int a, int b){
+void le(float *a, float *b){
     printf("Quais os números? (Ex: 13 34)\n");
-    scanf ("%d %d", &a, &b); 
+    scanf ("%f %f", a, b); 
 }
 
-float subtracao(float *a,float *b,float *c){
-    printf("Digite os números que serão subtraídos: ");
-    scanf("%f %f", a, b);
-    (*c) = (*a) - (*b);
-    return (*c);
+float subtracao(float a,float b){
+
+return a - b;
 }
 
 int main()
@@ -39,10 +37,14 @@ int main()
         printf("[1]Soma\n[2]Subtração\n[3]Multiplicação\n[4]Divisão\n[5]Raiz Quadrada\n[6]Potência\n");
 
         scanf("%d", &opcao);
+        
+        system("cls");
+        le(&a,&b);
 
         switch (opcao)
         {
-        case 2: c = subtracao(&a,&b,&c);
+        case 2: 
+        c = subtracao(a,b);
         imp(a,b,c,'-');
 
             break;
